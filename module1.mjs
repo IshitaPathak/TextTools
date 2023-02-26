f0a52c868e90d1d7195f2553cb88c2dcf7c05efa
IshitaPathak <pathakishita111@gmail.com>
2023-02-26 16:37:14 +0530

    deleting unused files

--- module1.mjs
+++
@@ -1,7 +0,0 @@
-// import { a } from "./module2.mjs";
-import demo,{a,b,c} from './module2.mjs';
-//we use curly braces when something is exported from other module not by default
-console.log(demo);
-console.log(a);
-console.log(b);
-console.log(c);
--- module2.mjs
+++
@@ -1,9 +0,0 @@
-const a="Ishita";
-const b="Manas";
-const c="Ishu";
-const d="Harry";
-
-export default c;
-export {a};
-export {b};
-export {c};
