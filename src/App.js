3185f66368aa69c4287e480f08de4f19fd63da35
IshitaPathak <pathakishita111@gmail.com>
2024-01-12 21:10:12 +0530

    fixing deployment failed checks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import About from './components/About';
+// import About from './components/About';
 import Navbar from './components/Navbar.js';
 import Textform from './components/Textform.js';
 import Alert from './components/Alert.js';
